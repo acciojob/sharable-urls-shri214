8389929ab1daca7a71e18a12d7839bfa20df338d
Shriram kushwaha <117708615+shri214@users.noreply.github.com>
2023-03-29 10:21:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // your code here
+ function updateUrl() {
+        var nameInput = document.getElementById("name");
+        var yearInput = document.getElementById("year");
+        var urlOutput = document.getElementById("url");
+        var name = nameInput.value;
+        var year = yearInput.value;
+        var queryString = "?";
+        if (name) {
+          queryString += "name=" + encodeURIComponent(name) + "&";
+        }
+        if (year) {
+          queryString += "year=" + encodeURIComponent(year) + "&";
+        }
+        if (queryString.endsWith("&")) {
+          queryString = queryString.slice(0, -1);
+        }
+        urlOutput.textContent = "https://localhost:8080/" + queryString;
+      }
